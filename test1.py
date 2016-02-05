7101452af0bd4bd7af52ae5d0d7def58ade233d0
Brant Knudson <bknudson@us.ibm.com>
2016-02-05 13:18:43 -0600

    Start work on NG schema

--- test1.py
+++ test1.py
@@ -139,3 +139,78 @@ SAMPLE_V3_CATALOG = json.loads(SAMPLE_V3_CATALOG_JSON)
 
 
 jsonschema.validate(SAMPLE_V3_CATALOG['catalog'], V3_SCHEMA)
+
+
+NG_SCHEMA_JSON = """
+{
+  "title": "Next-Generation Service Catalog",
+  "type": "array",
+  "items": {
+    "type": "object",
+    "properties": {
+      "type": {"type": "string", "minLength": 1},
+      "name": {"type": "string", "minLength": 1},
+      "id": {"type": "string", "minLength": 1},
+      "endpoints": {
+        "type": "array",
+        "items": {
+          "type": "object",
+          "properties": {
+            "interface": {
+              "type": "string",
+              "enum": ["public", "admin", "internal"]
+            },
+            "region": {"type": "string"},
+            "region_id": {"type": "string"},
+            "url": {"type": "string"},
+            "id": {"type": "string"}
+          },
+          "required": ["interface", "region", "region_id", "url", "id"]
+        }
+      }
+    },
+    "required": ["type", "name", "id", "endpoints"]
+  }
+}
+"""
+
+NG_SCHEMA = json.loads(NG_SCHEMA_JSON)
+
+
+SAMPLE_NG_CATALOG_JSON = """
+{"catalog": [
+  {
+    "type": "identity",
+    "name": "keystone",
+    "id": "cdc3ae8870af44698e722547d660355b",
+    "endpoints": [
+      {
+        "interface": "internal",
+        "region": "RegionOne",
+        "region_id": "RegionOne",
+        "url": "http://192.168.122.239:5000/v3",
+        "id": "0e0b3d009aa04da0aee163e034dd6190"
+      },
+      {
+        "interface": "admin",
+        "region": "RegionOne",
+        "region_id": "RegionOne",
+        "url": "http://192.168.122.239:35357/v3",
+        "id": "1024e233d9a4460d83292f151b405cf5"
+      },
+      {
+        "interface": "public",
+        "region": "RegionOne",
+        "region_id": "RegionOne",
+        "url": "http://192.168.122.239:5000/v3",
+        "id": "1b4b82bb39b6487b8265748a6142104f"
+      }
+    ]
+  }
+] }
+"""
+
+SAMPLE_NG_CATALOG = json.loads(SAMPLE_NG_CATALOG_JSON)
+
+
+jsonschema.validate(SAMPLE_NG_CATALOG['catalog'], NG_SCHEMA)
